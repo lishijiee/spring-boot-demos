17172b2e8a61e540de545b5ab2c3b6bef72fac3d
lishijiee <2424782003@qq.com>
2021-12-15 00:15:46 +0800

    add redis

--- spring-boot-thymeleaf/src/main/java/com/lishijiee/controller/Hello.java
+++ spring-boot-thymeleaf/src/main/java/com/lishijiee/controller/Hello.java
@@ -1,2 +1,7 @@
-package com.lishijiee.controller;public class Hello {
+package com.lishijiee.controller;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
 }
