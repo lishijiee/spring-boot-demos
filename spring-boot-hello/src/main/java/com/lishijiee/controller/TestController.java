1b9ada322c39b3665b5c3bb9328770a048a4a210
lishijiee <2424782003@qq.com>
2022-04-06 20:27:28 +0800

    测试

--- spring-boot-hello/src/main/java/com/lishijiee/controller/TestController.java
+++ spring-boot-hello/src/main/java/com/lishijiee/controller/TestController.java
@@ -4,9 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/test")
 public class TestController {
-    @RequestMapping("/getSth.json")
+    @RequestMapping("/")
     public String getSth(){
         return "SUCCESS";
     }
